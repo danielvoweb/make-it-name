8363e08fe715a5b75e34104a4305e8fefe4d911d
Daniel Vo <dan@daniel-vo.com>
2021-01-10 16:49:38 -0600

    updated with passing tests for feature

--- bin/program.js
+++ bin/program.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
-const app = require('../src/index.js')
+const App = require('../src/index.js')
+const options = require('yargs/yargs')(process.argv.slice(2)).argv
 
-// TODO: Add dependencies to yargs
+var app = new App(options)
+app.print(console.log)
 
-app.print()
+// TODO: Clean-up test names
+// TODO: Update documentation for use with args
+// TODO: Test using powershell script updates
+// TODO: Push npm package
