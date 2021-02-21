ba1491b96f5f30a3d8379268ba19cc26dbbba5d9
Daniel Vo <dan@daniel-vo.com>
2021-02-21 14:20:31 -0600

    refactor(app): changes dependency structure
    * moves data dependency higher in class closer to constructor
    * refactors tests to accommodate
    * addition of cli automation tools and packages

--- src/app.test.js
+++ src/app.test.js
@@ -43,21 +43,19 @@ test('random() should handle undefined', () => {
 
 test('compose() should build name from adjective and authors for odd randoms', () => {
     app.defaultRandom = 1
-    const actual = app.compose(
-        expectedAdjective,
-        expectedAuthor,
-        expectedScientist
-    )
+    app.adjective = expectedAdjective
+    app.author = expectedAuthor
+    app.scientist = expectedScientist
+    const actual = app.compose()
     expect(actual.output).toBe(`${expectedAdjective}-${expectedAuthor}`)
 })
 
 test('compose() should build name from adjective and scientist for even randoms', () => {
     app.defaultRandom = 2
-    const actual = app.compose(
-        expectedAdjective,
-        expectedAuthor,
-        expectedScientist
-    )
+    app.adjective = expectedAdjective
+    app.author = expectedAuthor
+    app.scientist = expectedScientist
+    const actual = app.compose()
     expect(actual.output).toBe(`${expectedAdjective}-${expectedScientist}`)
 })
 
@@ -76,9 +74,10 @@ test('transform() should handle undefined arguments', () => {
 
 test('transform() should chain with compose()', () => {
     app.defaultRandom = 1
-    const actual = app
-        .compose(expectedAdjective, expectedAuthor, expectedScientist)
-        .transform()
+    app.adjective = expectedAdjective
+    app.author = expectedAuthor
+    app.scientist = expectedScientist
+    const actual = app.compose().transform()
 
     expect(actual.output).toBe(`${expectedAdjective}_${expectedAuthor}`)
 })
@@ -98,10 +97,10 @@ test('capitalize() should handle undefined', () => {
 
 test('capitalize() should chain with compose() and transform()', () => {
     app.defaultRandom = 1
-    const actual = app
-        .compose(expectedAdjective, expectedAuthor, expectedScientist)
-        .capitalize()
-        .transform()
+    app.adjective = expectedAdjective
+    app.author = expectedAuthor
+    app.scientist = expectedScientist
+    const actual = app.compose().capitalize().transform()
 
     expect(actual.output).toBe(
         `${expectedCapitalizedAdjective}_${expectedCapitalizedAuthor}`
@@ -130,6 +129,7 @@ test('print() should log random name with all options', () => {
     const options = {
         u: true,
         c: true,
+        a: true,
     }
     app = new App(options)
     app.print(log)
@@ -149,4 +149,55 @@ test('print() should log a random name with options', () => {
 test('print() should log a random name with hyphens by default', () => {
     app.print(log)
     expect(log.mock.calls[0][0]).toMatch(/\w+-\w+/)
+})
+
+test('print() should set alliterative random names with options', () => {
+    const options = {
+        a: true,
+    }
+
+    app = new App(options)
+    app.print(log)
+    const actual = app.output.split('-')
+    expect(actual[0].substring(0, 1)).toBe(actual[1].substring(0, 1))
+})
+
+test('setAlliterativeData() should set author and scientist from adjective', () => {
+    const adjectives = ['brainy']
+    const authors = ['austen', 'bronte', 'christie']
+    const scientists = ['asclepius', 'brahe', 'cavendish']
+
+    app.setAlliterativeData(adjectives, authors, scientists)
+
+    expect(app.author).toBe(authors[1])
+    expect(app.scientist).toBe(scientists[1])
+})
+
+test('messy-maugham', () => {
+    const adjectives = ['cumbersome', 'efficacious']
+    const authors = ['austen', 'bronte', 'christie']
+    const scientists = ['asclepius', 'brahe', 'cavendish']
+
+    app.random = () => 1
+
+    app.setAlliterativeData(adjectives, authors, scientists)
+
+    expect(app.author).toBe(authors[2])
+    expect(app.scientist).toBe(scientists[2])
+})
+
+test('alliterate() should filter a collection by a matched character', () => {
+    const collection = ['austen', 'bronte', 'christie']
+    const actual = app.alliterate('b', collection)
+    expect(actual).toBe(collection[1])
+})
+
+test('aloof-milton', () => {
+    const collection = ['austen', 'bronte', 'christie']
+    const actual = app.alliterate('d', collection)
+    expect(actual).toBe(undefined)
+})
+
+test('bad-hawthorne', () => {
+    const collection = ['austen', 'bronte', 'christie']
 })
--- src/index.js
+++ src/index.js
@@ -1,27 +1,54 @@
 const { adjectives, authors, scientists } = require('./data')
 
 class App {
-    constructor(options) {
-        this.options = options || { u: false, c: false }
+    constructor(options, _adjectives, _authors, _scientists) {
+        this.options = options || { u: false, c: false, a: false }
         this.output = ''
         this.defaultRandom = new Date().getTime()
-        this.adjective = adjectives[this.random(adjectives.length)]
-        this.author = authors[this.random(authors.length)]
-        this.scientist = scientists[this.random(scientists.length)]
+        this.adjectives = _adjectives || adjectives
+        this.authors = _authors || authors
+        this.scientists = _scientists || scientists
+    }
+
+    setData() {
+        this.adjective = this.adjectives[this.random(this.adjectives.length)]
+        this.author = this.authors[this.random(this.authors.length)]
+        this.scientist = this.scientists[this.random(this.scientists.length)]
+    }
+
+    setAlliterativeData() {
+        this.adjective = this.adjectives[this.random(this.adjectives.length)]
+        const match = this.adjective.substring(0, 1)
+        this.author = this.alliterate(match, this.authors)
+        this.scientist = this.alliterate(match, this.scientists)
+    }
+
+    setNoun() {
+        return (this.nounType = ['author', 'scientist'](
+            this.defaultRandom % 2 == 0
+        ))
+    }
+
+    alliterate(match, collection) {
+        const filteredCollection = collection.filter(
+            (x) => x.substring(0, 1) == match
+        )
+        return filteredCollection[this.random(filteredCollection.length)]
     }
 
     print(log) {
-        this.compose(this.adjective, this.author, this.scientist)
+        this.options.a ? this.setAlliterativeData() : this.setData()
+        this.compose()
         if (this.options.c) this.capitalize()
         if (this.options.u) this.transform()
         log(this.output)
     }
 
-    compose(adjective, author, scientist) {
+    compose() {
         this.output =
             this.defaultRandom % 2 == 0
-                ? `${adjective}-${scientist}`
-                : `${adjective}-${author}`
+                ? `${this.adjective}-${this.scientist}`
+                : `${this.adjective}-${this.author}`
         return this
     }
 
